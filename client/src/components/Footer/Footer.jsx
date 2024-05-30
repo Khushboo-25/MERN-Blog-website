5580d871923bcb836a71321b8241e3f4277e079b
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2024-05-31 01:55:15 +0530

    Update Footer.jsx

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -15,8 +15,8 @@ const Footer = () => {
           <h2>Quick Links</h2>
           <ul>
             <li><a href="/home">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <li><a href="/home">About</a></li>
+            <li><a href="/home">Contact</a></li>
             <li><a href="/write">Write</a></li>
           </ul>
         </div>
